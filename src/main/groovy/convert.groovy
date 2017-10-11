35144193740054ea7c86b1b7e05671d241416e44
j136ifc <karl.kauc@sparinvest.com>
2017-10-11 13:02:25 +0200

    Beispiel geht

--- src/main/groovy/convert.groovy
+++ src/main/groovy/convert.groovy
@@ -1,10 +1,16 @@
 import groovy.xml.MarkupBuilder
 import groovy.xml.XmlUtil
+
+import javax.xml.bind.DatatypeConverter
 import java.util.logging.Logger
 
 def originDir = new File(".")
-def xmlDataSuppliere = "ESK"
-def xmlContentDate = new Date().format('YYYY-MM-dd')
+
+def xmlSystemCountry = 'AT'
+def xmlDataSuppliereShort = "ESK"
+def xmlDataSuppliereName = "ESPA"
+def xmlDataSuppliereType = 'IC'
+Calendar today = Calendar.getInstance();
 
 Logger log = Logger.getLogger("")
 
@@ -19,116 +25,30 @@ originDir.eachFile { file ->
         log.info("processing file: " + file.toPath())
 
         file.eachLine { line, count ->
-            if (count == 1)
+            if (count == 1) // überprüfen auf Überschriftzeile
                 return
 
             def split = line.split(';')
-            println "groesse: " + split.size()
-
-            def uniqueDocumentId = xmlDataSuppliere + "-" + new Date().format('YYYY-MM-dd') + "-" + generator((('A'..'Z') + ('0'..'9')).join(), 9)
-            def isin = split.getAt(0)
-
-            // General Financial Instrument information
-            def Financial_Instrument_Identifying_Data = split[0]
-            def Type_Of_Identification_Code_For_The_Financial_Instrument = split[1]
-            def Financial_Instrument_Name = split.getAt(2)
-            def Financial_Instrument_Currency = split?.getAt(3)
-            def Reporting_Date = split?.getAt(4)
-            def Financial_Instrument_Legal_Structure = split?.getAt(5)
-            def Financial_Instrument_Issuer_Name = split?.getAt(6)
-            def Financial_Instrument_Guarantor_Name = split.getAt(7)
-            def Product_Category_or_Nature = split.getAt(8)
-            def Leveraged_Financial_Instrument_or_Contingent_Liability_Instrument = split.getAt(9)
-
-            // Target markets - Investor Type
-            def Investor_Type_Retail
-            def Investor_Type_Professional
-            def Investor_Type_Eligible_Counterparty
-
-            // Target markets - Knowledge and / Or Experience
-            def Basic_Investor
-            def Informed_Investor
-            def Advanced_Investor
-            def Expert_Investor_Germany
-
-            // Target markets - Ability To Bear Losses
-            def No_Capital_Loss
-            def Limited_Capital_Loss
-            def Limited_Capital_Loss_Level
-            def No_Capital_Guarantee
-            def Loss_Beyond_Capital
-
-            // Target markets - Risk Tolerance
-            def Risk_Tolerance_PRIIPS_Methodology
-            def Risk_Tolerance_UCITS_Metholodology
-            def Risk_Tolerance_Internal_Methodology_For_Non_PRIIPS_and_Non_UCITS
-            def Risk_Tolerance_For_Non_PRIIPS_and_Non_UCITS_Spain
-            def Not_For_Investors_With_The_Lowest_Risk_Tolerance_Germany
-
-            // Target markets - Client Objectives & Needs
-            def Return_Profile_Preservation
-            def Return_Profile_Growth
-            def Return_Profile_Income
-            def Return_Profile_Hedging
-            def Option_or_Leveraged_Return_Profile
-            def Return_Profile_Other
-            def Return_Profile_Pension_Scheme_Germany
-            def Time_Horizon
-            def Maturity_Date
-            def May_Be_Terminated_Early
-            def Specific_Investment_Need
-
-            // Distribution strategy
-            def Execution_Only
-            def Execution_With_Appropriateness_Test_Or_Non_Advised_Services
-            def Investment_Advice
-            def Portfolio_Management
-
-            // Costs & Charges ex ante
-            def Structured_Securities_Quotation
-            def One_off_cost_Financial_Instrument_entry_cost
-            def One_off_cost_Financial_Instrument_maximum_entry_cost_fixed_amount_Italy
-            def One_off_cost_Financial_Instrument_maximum_entry_cost_acquired
-            def One_off_costs_Financial_Instrument_maximum_exit_cost
-            def One_off_costs_Financial_Instrument_maximum_exit_cost_fixed_amount_Italy
-            def One_off_costs_Financial_Instrument_maximum_exit_cost_acquired
-            def One_off_costs_Financial_Instrument_Typical_exit_cost
-            def One_off_cost_Financial_Instrument_exit_cost_structured_securities_prior_RHP
-            def Financial_Instrument_Ongoing_costs
-            def Financial_Instrument_Management_fee
-            def Financial_Instrument_Distribution_fee
-            def Financial_Instrument_Transaction_costs_ex_ante
-            def Financial_Instrument_Incidental_costs_ex_ante
-
-            // Costs & Charges ex post
-            def One_off_cost_Structured_Securities_entry_cost_ex_post
-            def One_off_costs_Structured_Securities_exit_cost_ex_post
-            def Financial_Instrument_Ongoing_costs_ex_post
-            def Structured_Securities_Ongoing_costs_ex_post_accumulated
-            def Financial_Instrument_Management_fee_ex_post
-            def Financial_Instrument_Distribution_fee_ex_post
-            def Financial_Instrument_Transaction_costs_ex_post
-            def Financial_Instrument_Incidental_costs_ex_post
-            def Beginning_Of_Calculation_Period
-            def End_Of_Calculation_Period
-
-            // WM additionals
-
+            println "groesse: " + split.size() // raus finden ob mit oder ohne wm additionals
+            def uniqueDocumentId = xmlDataSuppliereShort + "-" + new Date().format('YYYY-MM-dd') + "-" + generator((('A'..'Z') + ('0'..'9')).join(), 9)
+            def reportingDate = split[4]
+            def outputFileName = split[0] + "_" + reportingDate + "_" + xmlDataSuppliereShort + ".xml"
 
             def writer = new StringWriter()
             def fXML = new MarkupBuilder(writer)
 
-            fXML.FundsXML4(["xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance", "xsi:noNamespaceSchemaLocation": "https://fdp-service.oekb.at/FundsXML_4.0.1_AI.xsd"]) {
+            fXML.FundsXML4(["xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance", "xsi:noNamespaceSchemaLocation": "C:\\Data\\src\\FundsXML\\4.1.0\\FundsXML_4.1.0.xsd"]) {
+                // https://fdp-service.oekb.at/FundsXML_4.0.1_AI.xsd
                 ControlData {
                     UniqueDocumentID(uniqueDocumentId)
-                    DocumentGenerated(new Date().format('YYYY-MM-dd'))
+                    DocumentGenerated(DatatypeConverter.printDateTime(today))
                     Version("4.1.0")
-                    ContentDate(xmlContentDate)
+                    ContentDate(reportingDate)
                     DataSupplier {
-                        SystemCountry('AT')
-                        Short(xmlDataSuppliere)
-                        Name(xmlDataSuppliere)
-                        Type('company')
+                        SystemCountry(xmlSystemCountry)
+                        Short(xmlDataSuppliereShort)
+                        Name(xmlDataSuppliereName)
+                        Type(xmlDataSuppliereType)
                     }
                 }
                 RegulatoryReportings {
@@ -140,6 +60,139 @@ originDir.eachFile { file ->
                                         ISIN(split[0])
                                     }
                                 }
+                                DataSupplier {
+                                    SystemCountry(xmlSystemCountry)
+                                    Short(xmlDataSuppliereShort)
+                                    Name(xmlDataSuppliereName)
+                                    Type(xmlDataSuppliereType)
+                                }
+                                GeneralInformation {
+                                    Code(split[0])
+                                    CodificationSystem(split[1])
+                                    InstrumentName(split[2])
+                                    InstrumentCurrency(split[3])
+                                    ReportingDate(split[4])
+                                    LegalStructure(split[5])
+                                    IssuerName(split[6])
+                                    GuarantorName(split[7])
+                                    ProductCategoryNature(split[8])
+                                    LeveragedOrContingentLiability(split[9])
+                                }
+                                InvestorType {
+                                    Retail(split[10])
+                                    Professional(split[11])
+                                    EligibleCounterparty(split[12])
+                                }
+                                KnowledgeAndExperience {
+                                    BasicInvestor(split[13])
+                                    InformedInvestor(split[14])
+                                    AdvancedInvestor(split[15])
+                                    ExpertInvestorGermany(split[16])
+                                }
+                                AbilityToBearLosses {
+                                    NoCapitalLoss(split[17])
+                                    LimitedCapitalLoss(split[18])
+                                    LimitedCapitalLossLevel(split[19])
+                                    NoCapitalGuarantee(split[20])
+                                    LossBeyondCapital(split[21])
+                                }
+                                RiskTolerance {
+                                    PRIIPSMethodology(split[22])
+                                    UCITSMethodology(split[23])
+                                    InternalMethodology(split[24])
+                                    MethodologySpain(split[25])
+                                    NotForInvestorsWithLowestRiskToleranceGermany(split[26])
+                                }
+                                ClientObjectives {
+                                    ReturnProfile {
+                                        Preservation(split[27])
+                                        Growth(split[28])
+                                        Income(split[29])
+                                        Hedging(split[30])
+                                        OptionOrLeveraged(split[31])
+                                        Other(split[32])
+                                        PensionSchemeGermany(split[33])
+                                    }
+                                    TimeHorizon {
+                                        split[34] ? Years(split[34]) : Category(split[35])
+                                    }
+                                    MaturityDate(split[36])
+                                    MayBeTerminatedEarly(split[37])
+                                    SpecificInvestmentNeed(split[38])
+                                }
+                                DistributionStrategy {
+                                    ExecutionOnly(split[39])
+                                    ExecutionWithCheckOrNonAdvisedServices(split[40])
+                                    InvestmentAdvice(split[41])
+                                    PortfolioManagement(split[42])
+                                }
+                                CostsAndChargesExAnte {
+                                    if (split[44] != null && split[44] != "") {
+                                        // entweder fonds - oder structured security
+                                        Fund {
+                                            EntryCost(split[44])
+                                            MaxEntryCostItaly(split[46])
+                                            MaxEntryCostAcquired(split[47])
+                                            MaxExitCost(split[48])
+                                            MaxExitCostItaly(split[49])
+                                            MaxExitCostAcquired(split[50])
+                                            TypicalExitCost(split[51])
+                                            OngoingCosts(split[54])
+                                            ManagementFee(split[56])
+                                            DistributionFee(split[58])
+                                            TransactionCosts(split[59])
+                                            IncidentalCosts(split[60])
+                                        }
+                                    } else {
+                                        StructuredSecurity {
+                                            Quotation(split[43])
+                                            OneOfEntryCost(split[45])
+                                            TypicalExitCost(split[52])
+                                            ExitCostPriorRHP(split[53])
+                                            OngoingCosts(split[55])
+                                            ManagementFee(split[57])
+                                        }
+                                    }
+                                }
+                                CostsAndChargesExPost {
+                                    if (split[61] == null || split[61] == "") {
+                                        Fund {
+                                            OngoingCosts(split[63])
+                                            ManagementFee(split[66])
+                                            DistributionFee(split[68])
+                                            TransactionCosts(split[69])
+                                            IncidentialCosts(split[70])
+                                            CalculationPeriod {
+                                                Start(split[71])
+                                                End(split[72])
+                                            }
+                                        }
+                                    }
+                                    else {
+                                        StructuredSecurity {
+                                            EntryCost(split[61])
+                                            ExitCost(split[62])
+                                            OngoingCosts(split[64])
+                                            OngoingCostsAccumulated(split[65])
+                                            ManagementFee(split[67])
+                                        }
+                                    }
+                                }
+                                CountrySpecificData {
+                                    DE {
+                                        EMT_Additional {
+                                            TargetMarketInformation {
+                                                ModusApproval(split[73])
+                                                SpecialRequirementsDescription(split[74])
+                                                NatureProductCategory(split[75])
+                                                ApprovalProcess(split[76])
+                                            }
+                                            CostTransparency {
+                                                SwingPricing(split[77])
+                                            }
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
@@ -148,12 +201,14 @@ originDir.eachFile { file ->
             println "LINE:  " + line
             println XmlUtil.serialize(writer.toString())
 
+            if (new File(outputFileName).exists()) {
+                new File(outputFileName).delete()
+            }
+            new File(outputFileName).write(XmlUtil.serialize(writer.toString()))
+
         }
     } else {
         log.info(file.toPath().toString() + " ignoring... not a CSV file.")
     }
 
 }
-
-
-
